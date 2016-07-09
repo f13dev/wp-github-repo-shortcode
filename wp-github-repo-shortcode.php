786f9011805a59b5fe76eae204eaff3b7a5fa325
James Valentine <jv@f13dev.com>
2016-07-09 15:39:10 +0100

    Initial plugin file creation, header information and license

---
+++ wp-github-repo-shortcode.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name: GitHub Repository Shortcode
+Plugin URI: http://f13dev.com
+Description: This plugin enables you to enter shortcode on any page or post in your blog to show information and statistics about a repository on GitHub.
+Version: 1.0
+Author: Jim Valentine - f13dev
+Author URI: http://f13dev.com
+Text Domain: wp-github-repo-shortcode
+License: GPLv3
+*/
+
+/*
+Copyright 2016 James Valentine - f13dev (jv@f13dev.com)
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/
